98cdfd7c91537f9e1cc9bff0079bcdf425087cb5
Alex Verboon <alex.verboon@basevision.ch>
2020-04-08 08:55:43 +0200

    update

--- Build/build.ps1
+++ Build/build.ps1
@@ -52,7 +52,9 @@ Write-Host "Installing module " -ForegroundColor Green
 Find-module -Name "PSURLhaus" -Repository "LocalPSRepository" | Install-Module -Scope CurrentUser -Force
 
 Write-host  "Publishing Module into PSGallery" -ForegroundColor Green
-#Publish-Module -Path "$ModuleSourceDir" -NuGetApiKey  -Verbose 
+#Publish-Module -Path "$ModuleSourceDir" -NuGetApiKey <key>  -Verbose 
+
+
 
 
 
